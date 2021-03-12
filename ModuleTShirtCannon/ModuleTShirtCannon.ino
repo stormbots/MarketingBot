d77c16a3661ff19ef9b6b650ee0e9916ad87e296
tekdemo <tekdemo@gmail.com>
2021-03-11 20:17:39 -0800

    Minor Tshirt cannon cleanup

--- ModuleTShirtCannon/ModuleTShirtCannon.ino
+++ ModuleTShirtCannon/ModuleTShirtCannon.ino
@@ -1,11 +1,8 @@
 #include <ElapsedMillis.h>
 #include <PulsePosition.h>
 #include <Bounce.h>
-
 /* Hardware: */
 #define REVOLVER_MOTOR_PIN 1
-
-#define ANGLE_MOTOR_PIN 3
 
 #define MAG_ENCODER_A_PIN 1
 #define MAG_ENCODER_A_PIN 2
@@ -20,12 +17,9 @@
 #define CYLINDER_CLAMP_B_PIN 3
 
 #define INDEX_LOCK_PIN 3
-
 #define BUILT_IN_LED 13
 
-Bounce indexSwitch=Bounce(INDEX_LOCK_PIN,10);
-
-
+Bounce indexSwitch = Bounce(INDEX_LOCK_PIN,10);
 void setup(){
   pinMode(BUILT_IN_LED, OUTPUT);
 }
@@ -33,22 +27,22 @@ void setup(){
 
 void loop(){
   /* Check our system heartbeat */
-  if(heartbeat > 1000){
+  if (heartbeat > 1000){
     heartbeat=0;
-    digitalWrite(BUILT_IN_LED,!digitalRead(BUILT_IN_LED));
+    digitalWrite(BUILT_IN_LED, !digitalRead(BUILT_IN_LED));
   }
 
-  //TODO: Read radio inputs   
+  //TODO: Read radio inputs
   //TODO: Read safety signals somehow
 
   /* Process */
-  
+  bool cannonTrigger = radioInput.read(6) <= 1250;
 
-  run_state_machine();
 
+  run_state_machine(cannonTrigger);
 }
 
-// hardware: 
+// hardware:
 
 // up/down window motor, heavy reduction
 // has absolute position sensor mag encoder
@@ -62,22 +56,23 @@ void loop(){
 
 
 /**
-assume indexed and firing
+  assume indexed and firing
 
-engage cylinderClamp
-wait for pressure (timer probably)
-open pressureChamber pressure (if it's sufficiently pressurized)
-wait for pressureChamber to empty (timer again)
-//recovery
+  engage cylinderClamp
+  wait for pressure (timer probably)
+  open pressureChamber pressure (if it's sufficiently pressurized)
+  wait for pressureChamber to empty (timer again)
   seal pressureChamber (rebuild pressure, background)
   disengage cylinderClamp (some wait time)
   release indexLock (wait some time)
-//reloading
-start revolver
+  start revolver
   (wait some time to clear lock)
   engage indexLock
   wait for indexSwitch || max runtime exceeded
+
+
 */
+
 
 
 enum State{
@@ -89,45 +84,116 @@ enum State{
   RELOAD_UNLOCKED,
   RELOAD_LOCKED,
   RESET
-};
-State state=State.STARTUP;
-State last_state=State.RESET;
+}
 ElapsedMillis timer;
-void run_state_machine(){
-  
-  //Do pre-state machine loop process
+State state = State.STARTUP;
+State last_state=Stat.RESET;
+void run_state_machine(bool cannonTrigger){
+
   indexSwitch.update();
-
-
   switch(state){
     case STARTUP:
-      //run our code here
-      //lock index, 
+      //run code here
+      //lock index
       digitalWrite(INDEX_LOCK_A_PIN,true);
-      //open pressure chamber
-      //power ON motor?
-      analogWrite(REVOLVER_MOTOR_PIN, 32?);
-
-      if(switch.read()==false){
+      //motor on???
+      analogWrite(REVOLVER_MOTOR_PIN,127+32);
+      // open clamps
+      digitalWrite(CYLINDER_CLAMP_A_PIN,true);
+      //switch to pressurizing when it is ready to fire/ indexswitch is activated
+      indexSwitch.update();
+      if (indexSwitch.read()== false){
         state=PRESSURIZING;
       }
     break;
     case PRESSURIZING:
-      // do state things
-      if(timer > 3000){
-        state = IDLE;
+      //index locked
+      digitalWrite(INDEX_LOCK_A_PIN,true);
+      //motor off
+      analogWrite(REVOLVER_MOTOR_PIN,127);
+      //cylinder clamps open
+      digitalWrite(CYLINDER_CLAMP_A_PIN,true);
+      //dump valve closed
+      digitalWrite(DUMP_VALVE_A_PIN,false);
+      if (timer>3000){
+        state=IDLE;
       }
     break;
     case IDLE:
-    break;  
-  default:
-    //someone did something real weird
-    state=STARTUP;
+      //index locked
+      digitalWrite(INDEX_LOCK_A_PIN,true);
+      //motor off
+      analogWrite(REVOLVER_MOTOR_PIN,0);
+      //cylinder clamps closed
+      digitalWrite(CYLINDER_CLAMP_A_PIN,false);
+      //dump valve closed
+      digitalWrite(DUMP_VALVE_A_PIN,false);
+      // if the trigger switch if flicked progress to firing
+      if (cannonTrigger){
+        state = FIRING;
+      }
+    break;
+    case FIRING:
+      //index locked
+      digitalWrite(INDEX_LOCK_A_PIN,true);
+      //motor off
+      analogWrite(REVOLVER_MOTOR_PIN,127);
+      //cylinder clamps closed
+      digitalWrite(CYLINDER_CLAMP_A_PIN,false);
+      //dump valve open
+      digitalWrite(DUMP_VALVE_A_PIN,true);
+      // if the timer is up for firing and the switch is flicked back to avoid accidents advance to recovery
+      if (timer >3000 && cannonTrigger < 1250){
+        state = RECOVERY;
+      }
+    break;
+    case RECOVERY:
+      // index unlocked
+      digitalWrite(INDEX_LOCK_A_PIN,false);
+      // motor off
+      analogWrite(REVOLVER_MOTOR_PIN,127);
+      //cylinder clamps open
+      digitalWrite(CYLINDER_CLAMP_A_PIN,true);
+      // dump valve closed
+      digitalWrite(DUMP_VALVE_A_PIN,false);
+      // if timer expires advance, value is placeholder
+      if (timer > 3000){
+        state=RELOAD_UNLOCKED;
+      }
+    break;
+    case RELOAD_UNLOCKED:
+      //index unlocked
+      digitalWrite(INDEX_LOCK_A_PIN,false);
+      //motor on 
+      analogWrite(REVOLVER_MOTOR_PIN,127+32);//placeholder value 1/8 speed
+      //cylinder clamps open
+      digitalWrite(CYLINDER_CLAMP_A_PIN,true);
+      //dump valve closed
+      digitalWrite(DUMP_VALVE_A_PIN,false);
+      // if timer expires advance, value is placeholder
+      if (timer > 3000){
+        state=RELOAD_LOCKED;
+      }
+    break;
+    case RELOAD_LOCKED:
+      //index locked
+      digitalWrite(INDEX_LOCK_A_PIN,true);
+      //motor on
+      analogWrite(REVOLVER_MOTOR_PIN,127+32);//placeholder value 1/8 speed
+      //cylinder clamps open
+      digitalWrite(CYLINDER_CLAMP_A_PIN,true);
+      //dump valve closed
+      digitalWrite(DUMP_VALVE_A_PIN,false);
+      // if timer expires or switch is tripped advance, value is placeholder
+      if (timer > 3000||indexSwitch.read()== false){
+        state=PRESSURIZING;
+      }
   }
+  default:
+    //things got wack
+    state=STARTUP
 
-
-  //do post-state cleanup and checks
-  if(last_state != state){
+  if (last_state != state){
     timer=0;
     last_state=state;
   }
